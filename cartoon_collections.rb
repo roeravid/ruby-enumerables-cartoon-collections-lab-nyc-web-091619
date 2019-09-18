3dba548d7f08b2ea0d3e15fed9bc195764d7753a
Joe David <javid92@gmail.com>
2019-09-18 04:55:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array=[])
+  array.each_with_index{|dwarf, index| puts "#{index + 1}. *#{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array = [])
+  exclamation = array.each{|x| puts "#{x}!"}
+  exclamation
+   #newArray.each{planeteer_calls.upcase}
 end
 
 def long_planeteer_calls# code an argument here
